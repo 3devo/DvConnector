f01848a42433bcb2ffc1e7ac8e71e78f5550778a
Bob Thomas <bobthomas295@gmail.com>
2018-02-19 10:20:55 +0100

    FeConnector: Add tray icon and simple functionality
    * Tray icon with 3devo logo added.
    * Able to quit the app using the quit button
    * Able to open support page with button

---
+++ trayicon.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"./icon"
+	"github.com/getlantern/systray"
+	"github.com/skratchdot/open-golang/open"
+)
+
+func onReady() {
+	systray.SetIcon(icon.Data)
+	systray.SetTitle("3devo serial monitor")
+	mQuit := systray.AddMenuItem("Quit", "Quit the whole app")
+	go func() {
+		<-mQuit.ClickedCh
+		fmt.Println("Requesting quit")
+		systray.Quit()
+		fmt.Println("Finished quitting")
+		os.Exit(0)
+	}()
+
+	mAbout := systray.AddMenuItem("About", "About this application")
+	go func() {
+		<-mAbout.ClickedCh
+		open.Run("https://3devo.com/support/")
+	}()
+}
+
+func onExit() {
+	// clean up here
+}
