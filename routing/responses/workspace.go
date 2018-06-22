f683a75894eb6eb53b975e83a18aedd567c21415
Bob Thomas <bobthomas295@gmail.com>
2018-06-22 16:46:18 +0200

    REST: Add workspace swagger docs and implement response logic

---
+++ routing/responses/workspace.go
@@ -0,0 +1,50 @@
+package responses
+
+import (
+	"github.com/3devo/feconnector/models"
+	"github.com/3devo/feconnector/utils"
+	"github.com/asdine/storm/q"
+)
+
+// A single logFile response model
+//
+// This is used for returning a response with a single sheet object as body
+//
+// swagger:model WorkspaceResponse
+type WorkspaceResponse struct {
+	UUID   string           `json:"uuid"`
+	Title  string           `json:"title"`
+	Sheets []*SheetResponse `json:"sheets"`
+}
+
+// Parameters needed to create a sheet object
+// swagger:parameters CreateWorkspace UpdateWorkspace
+type WorkspaceCreationParams struct {
+	//in:body
+	Body struct {
+		UUID   string `json:"uuid"`
+		Title  string `json:"title"`
+		Sheets []int  `json:"sheets"`
+	}
+}
+
+// GenerateWorkSpaceResponse returns a WorkSpaceResponse object filled with actual sheet and chart data instead of id
+func GenerateWorkspaceResponseObject(workspace *models.Workspace, env *utils.Env) *WorkspaceResponse {
+	response := new(WorkspaceResponse)
+	var sheets []models.Sheet
+	var selection []q.Matcher
+	for _, sheet := range workspace.Sheets {
+		selection = append(selection, q.Eq("UUID", sheet))
+	}
+	query := env.Db.Select(q.Or(selection...))
+	query.Find(&sheets)
+	sheetResponses := make([]*SheetResponse, 0)
+	for _, sheet := range sheets {
+		sheetResponses = append(sheetResponses, GenerateSheetResponseObject(&sheet, env))
+	}
+
+	response.UUID = workspace.UUID
+	response.Title = workspace.Title
+	response.Sheets = sheetResponses
+	return response
+}
--- routing/workspace.go
+++ routing/workspace.go
@@ -6,46 +6,76 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"strconv"
 
 	"github.com/3devo/feconnector/models"
+	"github.com/3devo/feconnector/routing/responses"
 	"github.com/3devo/feconnector/utils"
 	"github.com/julienschmidt/httprouter"
 )
 
+// swagger:route GET /workspaces Workspaces GetAllWorkspaces
+//
+// Handler to retrieve all workspaces
+//
+// This will return all available workspaces
+//
+// Produces:
+//	application/json
+//
+// Responses:
+//        200: body:[]WorkspaceResponse
 func GetAllWorkspaces(env *utils.Env) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 		workspaces := make([]models.Workspace, 0)
-		responseObject := make([]models.WorkspaceResponse, 0)
+		responseObject := make([]*responses.WorkspaceResponse, 0)
 		query, _ := utils.QueryBuilder(env, r)
 		query.Find(&workspaces)
-		for _, Workspace := range workspaces {
-			responseObject = append(responseObject, Workspace.GetResponseObject(env))
+		for _, workspace := range workspaces {
+			responseObject = append(responseObject, responses.GenerateWorkspaceResponseObject(&workspace, env))
 		}
 		w.WriteHeader(http.StatusOK)
 		json.NewEncoder(w).Encode(responseObject)
 	}
 }
 
+// swagger:route GET /workspaces/{uuid} Workspaces GetWorkspace
+//
+// Handler to retrieve a single workspaces
+//
+// This will return a single workspace
+//
+// Produces:
+//	application/json
+//
+// Responses:
+//        200: body:WorkspaceResponse
 func GetWorkspace(env *utils.Env) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-		var Workspace models.Workspace
-		id, err := strconv.Atoi(ps.ByName("id"))
-		if err != nil {
-			http.Error(w, "id should be a number", http.StatusNotAcceptable)
-			return
-		}
-		err = env.Db.One("ID", id, &Workspace)
+		var workspace models.Workspace
+		uuid := ps.ByName("uuid")
+		err := env.Db.One("UUID", uuid, &workspace)
 		if err != nil {
 			log.Println(err)
 			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
 			return
 		}
 		w.WriteHeader(http.StatusOK)
-		json.NewEncoder(w).Encode(Workspace.GetResponseObject(env))
+		json.NewEncoder(w).Encode(responses.GenerateWorkspaceResponseObject(&workspace, env))
 	}
 }
 
+// swagger:route POST /workspaces/ Workspaces CreateWorkspace
+//
+// Handler to create a new workspace object
+//
+// This will add a new workspace to the database
+// Only sheets that exist can be added
+//
+// Produces:
+//	application/json
+//
+// Responses:
+//        200: StatusResponse
 func CreateWorkspace(env *utils.Env) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 		var Workspace models.Workspace
@@ -53,8 +83,8 @@ func CreateWorkspace(env *utils.Env) httprouter.Handle {
 
 		json.Unmarshal(body, &Workspace)
 		for _, sheetId := range Workspace.Sheets {
-			if env.Db.One("ID", sheetId, &models.Sheet{}) != nil {
-				http.Error(w, fmt.Sprintf("Sheet with id %v doesn't exist", sheetId), http.StatusConflict)
+			if env.Db.One("UUID", sheetId, &models.Sheet{}) != nil {
+				http.Error(w, fmt.Sprintf("Workspace with uuid %v doesn't exist", sheetId), http.StatusConflict)
 				return
 			}
 		}
@@ -68,26 +98,33 @@ func CreateWorkspace(env *utils.Env) httprouter.Handle {
 	}
 }
 
+// swagger:route PUT /workspaces/{uuid} Workspaces UpdateWorkspace
+//
+// Handler to update an existing workspace object
+//
+// This will update an existing workspace object
+// Only sheets that exist can be added
+//
+// Produces:
+//	application/json
+//
+// Responses:
+//        200: StatusResponse
 func UpdateWorkspace(env *utils.Env) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 		var Workspace models.Workspace
 		body, _ := ioutil.ReadAll(r.Body)
-		id, err := strconv.Atoi(ps.ByName("id"))
-		if err != nil {
-			http.Error(w, "id should be a number", http.StatusNotAcceptable)
-			return
-		}
-		Workspace.ID = id
+		uuid := ps.ByName("uuid")
 		json.Unmarshal(body, &Workspace)
-		err = env.Db.One("ID", id, &models.Workspace{})
+		err := env.Db.One("UUID", uuid, &models.Workspace{})
 		if err != nil {
 			log.Println(err)
 			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
 			return
 		}
 		for _, sheetId := range Workspace.Sheets {
-			if env.Db.One("ID", sheetId, &models.Sheet{}) != nil {
-				http.Error(w, fmt.Sprintf("Sheet with id %v doesn't exist", sheetId), http.StatusConflict)
+			if env.Db.One("UUID", sheetId, &models.Sheet{}) != nil {
+				http.Error(w, fmt.Sprintf("Workspace with uuid %v doesn't exist", sheetId), http.StatusConflict)
 				return
 			}
 		}
@@ -96,20 +133,27 @@ func UpdateWorkspace(env *utils.Env) httprouter.Handle {
 			http.Error(w, http.StatusText(http.StatusConflict), http.StatusConflict)
 		} else {
 			w.WriteHeader(http.StatusOK)
-			fmt.Fprintf(w, "Updated Workspace with ID %v without error", id)
+			fmt.Fprintf(w, "Updated Workspace with ID %v without error", uuid)
 		}
 	}
 }
 
+// swagger:route DELETE /workspaces/{uuid} Workspaces DeleteWorkspace
+//
+// Handler to delete a existing workspace object
+//
+// This will add delete a workspace from the database
+//
+// Produces:
+//	application/json
+//
+// Responses:
+//        200: StatusResponse
 func DeleteWorkspace(env *utils.Env) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 		var Workspace models.Workspace
-		id, err := strconv.Atoi(ps.ByName("id"))
-		if err != nil {
-			http.Error(w, "id should be a number", http.StatusNotAcceptable)
-			return
-		}
-		err = env.Db.One("ID", id, &Workspace)
+		uuid := ps.ByName("uuid")
+		err := env.Db.One("UUID", uuid, &Workspace)
 		if err != nil {
 			log.Println(err)
 			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
@@ -121,6 +165,6 @@ func DeleteWorkspace(env *utils.Env) httprouter.Handle {
 			return
 		}
 		w.WriteHeader(http.StatusOK)
-		fmt.Fprintf(w, "Removed Workspace with %v successfully", id)
+		fmt.Fprintf(w, "Removed Workspace with %v successfully", uuid)
 	}
 }
