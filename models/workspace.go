7861a0ad6f9ec1775f8c1afd87531846e1ee9dbb
Bob Thomas <bobthomas295@gmail.com>
2018-06-26 13:37:11 +0200

    MODELS: Add new validation rules to the workspace model

--- models/workspace.go
+++ models/workspace.go
@@ -1,7 +1,7 @@
 package models
 
 type Workspace struct {
-	UUID   string   `storm:"id" json:"uuid"`
-	Title  string   `json:"title"`
-	Sheets []string `json:"sheets"`
+	UUID   string   `storm:"id" json:"uuid" validate:"uuid"`
+	Title  string   `json:"title" validate:"required"`
+	Sheets []string `json:"sheets" validate:"dive,sheet-exists"`
 }
