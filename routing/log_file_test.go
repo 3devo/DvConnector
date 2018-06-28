7aa3a6ef6282733552c864d57466c80429961caf
Bob Thomas <bobthomas295@gmail.com>
2018-06-28 13:47:07 +0200

    TESTING: Added 92% coverage of the logFile route
    * the other percentage of coverage is checking failure of the model
    functions. And I don't know how to mock those yet

--- routing/log_file_test.go
+++ routing/log_file_test.go
@@ -2,14 +2,19 @@ package routing
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http/httptest"
 	"os"
+	"path"
 	"path/filepath"
+	"strings"
 	"testing"
+	"time"
 
 	"github.com/3devo/feconnector/models"
 	"github.com/3devo/feconnector/routing/responses"
+	uuid "github.com/satori/go.uuid"
 
 	"github.com/3devo/feconnector/utils"
 	"github.com/asdine/storm"
@@ -39,7 +44,7 @@ func TestGetSingle(t *testing.T) {
 	dir, db := prepareDB()
 	defer os.RemoveAll(dir)
 	defer db.Close()
-	env := &utils.Env{Db: db, Validator: validator.New()}
+	env := &utils.Env{Db: db, Validator: validator.New(), FileDir: path.Dir(dir)}
 
 	Convey("Given a HTTP request for /api/x/logFiles/550e8400-e29b-41d4-a716-446655440000", t, func() {
 
@@ -54,9 +59,10 @@ func TestGetSingle(t *testing.T) {
 
 			Convey("Then the response should be a 200 because the item exists in the db", func() {
 				result := resp.Result()
-				So(result.StatusCode, ShouldEqual, 200)
 				body, _ := ioutil.ReadAll(result.Body)
 				expected, _ := json.Marshal(responses.GenerateLogResponse(&logFiles[0], env))
+
+				So(result.StatusCode, ShouldEqual, 200)
 				So(body, ShouldResemble, append(expected, 10))
 			})
 		})
@@ -75,13 +81,14 @@ func TestGetSingle(t *testing.T) {
 			Convey("Then the response should be a 404 with correct response body", func() {
 				result := resp.Result()
 				body, _ := ioutil.ReadAll(result.Body)
-				So(result.StatusCode, ShouldEqual, 404)
 				response := responses.ResourceStatusResponse{}
 				response.Body.Code = 404
 				response.Body.Resource = "Logfiles"
 				response.Body.Action = "GET"
 				response.Body.Error = "not found"
 				expected, _ := json.Marshal(response.Body)
+
+				So(result.StatusCode, ShouldEqual, 404)
 				So(string(body), ShouldResemble, string(append(expected, 10)))
 			})
 		})
@@ -92,7 +99,7 @@ func TestGetMultiple(t *testing.T) {
 	dir, db := prepareDB()
 	defer os.RemoveAll(dir)
 	defer db.Close()
-	env := &utils.Env{Db: db, Validator: validator.New()}
+	env := &utils.Env{Db: db, Validator: validator.New(), FileDir: path.Dir(dir)}
 
 	Convey("Given a HTTP request for api/x/logFiles", t, func() {
 		router := httprouter.New()
@@ -103,13 +110,14 @@ func TestGetMultiple(t *testing.T) {
 		router.ServeHTTP(resp, req)
 		Convey("Then the response should return 200 with the 3 logFiles", func() {
 			result := resp.Result()
-			So(result.StatusCode, ShouldEqual, 200)
 			body, _ := ioutil.ReadAll(result.Body)
 			logResponses := make([]*responses.LogFileResponse, 0)
 			for _, model := range logFiles {
 				logResponses = append(logResponses, responses.GenerateLogResponse(&model, env))
 			}
 			expected, _ := json.Marshal(logResponses)
+
+			So(result.StatusCode, ShouldEqual, 200)
 			So(body, ShouldResemble, append(expected, 10))
 		})
 	})
@@ -127,11 +135,12 @@ func TestGetMultiple(t *testing.T) {
 		router.ServeHTTP(resp, req)
 		Convey("Then the response should return 200 with the 1 logFiles", func() {
 			result := resp.Result()
-			So(result.StatusCode, ShouldEqual, 200)
 			body, _ := ioutil.ReadAll(result.Body)
 			logResponses := make([]*responses.LogFileResponse, 0)
 			logResponses = append(logResponses, responses.GenerateLogResponse(&logFiles[0], env))
 			expected, _ := json.Marshal(logResponses)
+
+			So(result.StatusCode, ShouldEqual, 200)
 			So(body, ShouldResemble, append(expected, 10))
 		})
 	})
@@ -149,13 +158,14 @@ func TestGetMultiple(t *testing.T) {
 		router.ServeHTTP(resp, req)
 		Convey("Then the response should return 200 with the 2 logFiles", func() {
 			result := resp.Result()
-			So(result.StatusCode, ShouldEqual, 200)
 			body, _ := ioutil.ReadAll(result.Body)
 			logResponses := make([]*responses.LogFileResponse, 0)
 			for _, model := range logFiles[1:] {
 				logResponses = append(logResponses, responses.GenerateLogResponse(&model, env))
 			}
 			expected, _ := json.Marshal(logResponses)
+
+			So(result.StatusCode, ShouldEqual, 200)
 			So(body, ShouldResemble, append(expected, 10))
 		})
 	})
@@ -173,11 +183,12 @@ func TestGetMultiple(t *testing.T) {
 		router.ServeHTTP(resp, req)
 		Convey("Then the response should return 200 with the 2 logFiles", func() {
 			result := resp.Result()
-			So(result.StatusCode, ShouldEqual, 200)
 			body, _ := ioutil.ReadAll(result.Body)
 			logResponses := make([]*responses.LogFileResponse, 0)
 			logResponses = append(logResponses, responses.GenerateLogResponse(&logFiles[0], env))
 			expected, _ := json.Marshal(logResponses)
+
+			So(result.StatusCode, ShouldEqual, 200)
 			So(body, ShouldResemble, append(expected, 10))
 		})
 	})
@@ -195,13 +206,14 @@ func TestGetMultiple(t *testing.T) {
 		router.ServeHTTP(resp, req)
 		Convey("Then the response should return 200 with the 3 logFiles", func() {
 			result := resp.Result()
-			So(result.StatusCode, ShouldEqual, 200)
 			body, _ := ioutil.ReadAll(result.Body)
 			logResponses := make([]*responses.LogFileResponse, 0)
 			for _, model := range logFiles {
 				logResponses = append(logResponses, responses.GenerateLogResponse(&model, env))
 			}
 			expected, _ := json.Marshal(logResponses)
+
+			So(result.StatusCode, ShouldEqual, 200)
 			So(body, ShouldResemble, append(expected, 10))
 		})
 	})
@@ -220,65 +232,327 @@ func TestGetMultiple(t *testing.T) {
 		router.ServeHTTP(resp, req)
 		Convey("Then the response should return 200 with the 3 logFiles", func() {
 			result := resp.Result()
-			So(result.StatusCode, ShouldEqual, 200)
 			body, _ := ioutil.ReadAll(result.Body)
 			logResponses := make([]*responses.LogFileResponse, 0)
 			for _, model := range logFiles {
 				logResponses = append(logResponses, responses.GenerateLogResponse(&model, env))
 			}
-			for i := len(logResponses)/2 - 1; i >= 0; i-- {
-				opp := len(logResponses) - 1 - i
-				logResponses[i], logResponses[opp] = logResponses[opp], logResponses[i]
+			reversed := [3]*responses.LogFileResponse{
+				logResponses[2],
+				logResponses[1],
+				logResponses[0],
 			}
-			expected, _ := json.Marshal(logResponses)
+			expected, _ := json.Marshal(reversed)
+
+			So(result.StatusCode, ShouldEqual, 200)
 			So(body, ShouldResemble, append(expected, 10))
 		})
 	})
 }
 
 func TestCreate(t *testing.T) {
-	// dir, db := prepareDB()
-	// defer os.RemoveAll(dir)
-	// defer db.Close()
-	// env := &utils.Env{Db: db, Validator: validator.New()}
+	Convey("Setup", t, func() {
+		dir, db := prepareDB()
+		defer os.RemoveAll(dir)
+		defer db.Close()
+		env := &utils.Env{Db: db, Validator: validator.New(), FileDir: dir}
 
-	// Convey("Given a HTTP POST request for api/x/logFiles", t, func() {
-	// 	env.Validator.RegisterValidation("uuid", func(fl validator.FieldLevel) bool {
-	// 		return utils.IsValidUUID(fl.Field().String())
-	// 	})
+		updateBody := &responses.LogFileUpdateBody{}
+		updateBody.Data.UUID = uuid.Must(uuid.NewV4()).String()
+		updateBody.Data.Name = "test"
+		updateBody.Data.Note = "test note"
 
-	// 	router := httprouter.New()
-	// 	router.POST("/api/x/logFiles", CreateLogFile(env))
-	// 	updateBody := &responses.LogFileUpdateBody{}
-	// 	updateBody.Data.UUID = uuid.Must(uuid.NewV4()).String()
-	// 	updateBody.Data.Name = "test"
-	// 	updateBody.Data.Note = "test note"
-	// 	requestBody, _ := json.Marshal(updateBody.Data)
-	// 	req := httptest.NewRequest("POST", "/api/x/logFiles", strings.NewReader(string(requestBody)))
-	// 	resp := httptest.NewRecorder()
+		env.Validator.RegisterValidation("uuid", func(fl validator.FieldLevel) bool {
+			return utils.IsValidUUID(fl.Field().String())
+		})
 
-	// 	router.ServeHTTP(resp, req)
+		router := httprouter.New()
+		Convey("Given a HTTP POST request for api/x/logFiles with a valid body", func() {
+			router.POST("/api/x/logFiles", CreateLogFile(env))
+			requestBody, _ := json.Marshal(updateBody.Data)
+			req := httptest.NewRequest("POST", "/api/x/logFiles", strings.NewReader(string(requestBody)))
+			resp := httptest.NewRecorder()
 
-	// 	Convey("Then the response should be a success status and a new logFile should have been created", func() {
-	// 		result := resp.Result()
-	// 		body, _ := ioutil.ReadAll(result.Body)
-	// 		log.Println(string(requestBody), string(body))
-	// 		So(result.StatusCode, ShouldEqual, 200)
-	// 		response := responses.ResourceStatusResponse{}
-	// 		response.Body.Code = 200
-	// 		response.Body.Resource = "Logfiles"
-	// 		response.Body.Action = "CREATE"
-	// 		expected, _ := json.Marshal(response.Body)
-	// 		So(string(body), ShouldResemble, string(append(expected, 10)))
-	// 	})
-	// })
+			router.ServeHTTP(resp, req)
+
+			Convey("Then the response should be a success status and a new logFile should have been created", func() {
+				result := resp.Result()
+				body, _ := ioutil.ReadAll(result.Body)
+				response := responses.ResourceStatusResponse{}
+				response.Body.Code = 200
+				response.Body.Resource = "Logfiles"
+				response.Body.Action = "CREATE"
+				expected, _ := json.Marshal(response.Body)
+
+				So(result.StatusCode, ShouldEqual, 200)
+				So(string(body), ShouldResemble, string(append(expected, 10)))
+			})
+		})
+
+		Convey("Given a HTTP POST request for api/x/logFiles with an existing uuid", func() {
+			updateBody.Data.UUID = logFiles[0].UUID
+			router.POST("/api/x/logFiles", CreateLogFile(env))
+			requestBody, _ := json.Marshal(updateBody.Data)
+			req := httptest.NewRequest("POST", "/api/x/logFiles", strings.NewReader(string(requestBody)))
+			resp := httptest.NewRecorder()
+
+			router.ServeHTTP(resp, req)
+
+			Convey("Then the response should fail and return already exists error", func() {
+				result := resp.Result()
+				body, _ := ioutil.ReadAll(result.Body)
+				response := responses.ResourceStatusResponse{}
+				response.Body.Code = 500
+				response.Body.Resource = "Logfiles"
+				response.Body.Action = "CREATE"
+				response.Body.Error = fmt.Sprintf("Logfile with %v already exists", updateBody.Data.UUID)
+				expected, _ := json.Marshal(response.Body)
+
+				So(result.StatusCode, ShouldEqual, 500)
+				So(string(body), ShouldResemble, string(append(expected, 10)))
+			})
+		})
+
+		Convey("Given a HTTP POST request for api/x/logFiles with a invalid uuid in body", func() {
+			router.POST("/api/x/logFiles", CreateLogFile(env))
+			updateBody.Data.UUID = "invalid"
+			requestBody, _ := json.Marshal(updateBody.Data)
+			req := httptest.NewRequest("POST", "/api/x/logFiles", strings.NewReader(string(requestBody)))
+			resp := httptest.NewRecorder()
+
+			router.ServeHTTP(resp, req)
+
+			Convey("Then the response should be a internal server error with uuid validation fail error", func() {
+				result := resp.Result()
+				body, _ := ioutil.ReadAll(result.Body)
+				response := responses.ResourceStatusResponse{}
+				response.Body.Code = 500
+				response.Body.Resource = "Logfiles"
+				response.Body.Action = "CREATE"
+				response.Body.Error = "Key: 'LogFileUpdateBody.Data.UUID' Error:Field validation for 'UUID' failed on the 'uuid' tag"
+				expected, _ := json.Marshal(response.Body)
+
+				So(result.StatusCode, ShouldEqual, 500)
+				So(string(body), ShouldResemble, string(append(expected, 10)))
+			})
+		})
+
+		Convey("Given a HTTP POST request for api/x/logFiles with a missing name in body", func() {
+			router.POST("/api/x/logFiles", CreateLogFile(env))
+			updateBody.Data.Name = ""
+			requestBody, _ := json.Marshal(updateBody.Data)
+			req := httptest.NewRequest("POST", "/api/x/logFiles", strings.NewReader(string(requestBody)))
+			resp := httptest.NewRecorder()
+
+			router.ServeHTTP(resp, req)
+
+			Convey("Then the response should be a internal server error with required name message", func() {
+				result := resp.Result()
+				body, _ := ioutil.ReadAll(result.Body)
+				response := responses.ResourceStatusResponse{}
+				response.Body.Code = 500
+				response.Body.Resource = "Logfiles"
+				response.Body.Action = "CREATE"
+				response.Body.Error = "Key: 'LogFileUpdateBody.Data.Name' Error:Field validation for 'Name' failed on the 'required' tag"
+				expected, _ := json.Marshal(response.Body)
+
+				So(result.StatusCode, ShouldEqual, 500)
+				So(string(body), ShouldResemble, string(append(expected, 10)))
+			})
+		})
+	})
+}
+
+func TestUpdate(t *testing.T) {
+	Convey("Setup", t, func() {
+		dir, db := prepareDB()
+		defer os.RemoveAll(dir)
+		defer db.Close()
+		env := &utils.Env{Db: db, Validator: validator.New(), FileDir: path.Dir(dir)}
+
+		env.Validator.RegisterValidation("uuid", func(fl validator.FieldLevel) bool {
+			return utils.IsValidUUID(fl.Field().String())
+		})
+		updateBody := &responses.LogFileUpdateBody{}
+		updateBody.Data.UUID = logFiles[0].UUID
+		updateBody.Data.Name = logFiles[0].Name
+		updateBody.Data.Note = "test note"
+
+		router := httprouter.New()
+		Convey("Given a HTTP PUT request for api/x/logFiles/uuid with a valid body", func() {
+			router.PUT("/api/x/logFiles/:uuid", UpdateLogFile(env))
+			requestBody, _ := json.Marshal(updateBody.Data)
+			req := httptest.NewRequest("PUT", "/api/x/logFiles/"+updateBody.Data.UUID, strings.NewReader(string(requestBody)))
+			resp := httptest.NewRecorder()
+
+			router.ServeHTTP(resp, req)
+
+			Convey("Then the response should be a successful", func() {
+				result := resp.Result()
+				body, _ := ioutil.ReadAll(result.Body)
+				response := responses.ResourceStatusResponse{}
+				response.Body.Code = 200
+				response.Body.Resource = "Logfiles"
+				response.Body.Action = "UPDATE"
+				response.Body.Error = ""
+				expected, _ := json.Marshal(response.Body)
+
+				So(result.StatusCode, ShouldEqual, 200)
+				So(string(body), ShouldResemble, string(append(expected, 10)))
+			})
+		})
+
+		Convey("Given a HTTP PUT request for api/x/logFiles/uuid with a unknown uid", func() {
+			router.PUT("/api/x/logFiles/:uuid", UpdateLogFile(env))
+			updateBody.Data.UUID = "550e8400-e29b-41d4-a716-446655440004"
+			requestBody, _ := json.Marshal(updateBody.Data)
+			req := httptest.NewRequest("PUT", "/api/x/logFiles/"+updateBody.Data.UUID, strings.NewReader(string(requestBody)))
+			resp := httptest.NewRecorder()
+
+			router.ServeHTTP(resp, req)
+
+			Convey("Then the response should return a not found error", func() {
+				result := resp.Result()
+				body, _ := ioutil.ReadAll(result.Body)
+				response := responses.ResourceStatusResponse{}
+				response.Body.Code = 404
+				response.Body.Resource = "Logfiles"
+				response.Body.Action = "UPDATE"
+				response.Body.Error = "not found"
+				expected, _ := json.Marshal(response.Body)
+
+				So(result.StatusCode, ShouldEqual, 404)
+				So(string(body), ShouldResemble, string(append(expected, 10)))
+			})
+		})
+
+		Convey("Given a HTTP PUT request for api/x/logFiles/uuid with a invalid uuid in the body", func() {
+			router.PUT("/api/x/logFiles/:uuid", UpdateLogFile(env))
+			updateBody.Data.UUID = ""
+			requestBody, _ := json.Marshal(updateBody.Data)
+			updateBody.Data.UUID = logFiles[0].UUID
+			req := httptest.NewRequest("PUT", "/api/x/logFiles/"+updateBody.Data.UUID, strings.NewReader(string(requestBody)))
+			resp := httptest.NewRecorder()
+
+			router.ServeHTTP(resp, req)
+
+			Convey("Then the response should return a internal server error with a uuid validation error", func() {
+				result := resp.Result()
+				body, _ := ioutil.ReadAll(result.Body)
+
+				response := responses.ResourceStatusResponse{}
+				response.Body.Code = 500
+				response.Body.Resource = "Logfiles"
+				response.Body.Action = "UPDATE"
+				response.Body.Error = "Key: 'LogFileUpdateBody.Data.UUID' Error:Field validation for 'UUID' failed on the 'uuid' tag"
+				expected, _ := json.Marshal(response.Body)
+
+				So(result.StatusCode, ShouldEqual, 500)
+				So(string(body), ShouldResemble, string(append(expected, 10)))
+			})
+		})
+
+		Convey("Given a HTTP PUT request for api/x/logFiles/uuid with a missing name", func() {
+			router.PUT("/api/x/logFiles/:uuid", UpdateLogFile(env))
+			updateBody.Data.Name = ""
+
+			requestBody, _ := json.Marshal(updateBody.Data)
+			req := httptest.NewRequest("PUT", "/api/x/logFiles/"+updateBody.Data.UUID, strings.NewReader(string(requestBody)))
+			resp := httptest.NewRecorder()
+
+			router.ServeHTTP(resp, req)
+
+			Convey("Then the response should be a internal server error with name validation error", func() {
+				result := resp.Result()
+				body, _ := ioutil.ReadAll(result.Body)
+
+				response := responses.ResourceStatusResponse{}
+				response.Body.Code = 500
+				response.Body.Resource = "Logfiles"
+				response.Body.Action = "UPDATE"
+				response.Body.Error = "Key: 'LogFileUpdateBody.Data.Name' Error:Field validation for 'Name' failed on the 'required' tag"
+				expected, _ := json.Marshal(response.Body)
+
+				So(result.StatusCode, ShouldEqual, 500)
+				So(string(body), ShouldResemble, string(append(expected, 10)))
+			})
+		})
+	})
+}
+
+func TestDelete(t *testing.T) {
+	Convey("Setup", t, func() {
+		dir, db := prepareDB()
+		defer os.RemoveAll(dir)
+		defer db.Close()
+		env := &utils.Env{Db: db, Validator: validator.New(), FileDir: dir}
+		router := httprouter.New()
+		Convey("Given a HTTP DELETE request for api/x/logFiles/uuid with a known uuid", func() {
+			router.DELETE("/api/x/logFiles/:uuid", DeleteLogFile(env))
+
+			req := httptest.NewRequest("DELETE", "/api/x/logFiles/"+logFiles[0].UUID, nil)
+			resp := httptest.NewRecorder()
+
+			router.ServeHTTP(resp, req)
+
+			Convey("Then the response should be successful", func() {
+				result := resp.Result()
+				body, _ := ioutil.ReadAll(result.Body)
+
+				response := responses.ResourceStatusResponse{}
+				response.Body.Code = 200
+				response.Body.Resource = "Logfiles"
+				response.Body.Action = "DELETE"
+				response.Body.Error = ""
+				expected, _ := json.Marshal(response.Body)
+
+				So(result.StatusCode, ShouldEqual, 200)
+				So(string(body), ShouldResemble, string(append(expected, 10)))
+			})
+		})
+
+		Convey("Given a HTTP DELETE request for api/x/logFiles/uuid with a unknown uuid", func() {
+			router.DELETE("/api/x/logFiles/:uuid", DeleteLogFile(env))
+
+			req := httptest.NewRequest("DELETE", "/api/x/logFiles/unknown", nil)
+			resp := httptest.NewRecorder()
+
+			router.ServeHTTP(resp, req)
+
+			Convey("Then the response should fail and return not found", func() {
+				result := resp.Result()
+				body, _ := ioutil.ReadAll(result.Body)
+
+				response := responses.ResourceStatusResponse{}
+				response.Body.Code = 404
+				response.Body.Resource = "Logfiles"
+				response.Body.Action = "DELETE"
+				response.Body.Error = "not found"
+				expected, _ := json.Marshal(response.Body)
+
+				So(result.StatusCode, ShouldEqual, 404)
+				So(string(body), ShouldResemble, string(append(expected, 10)))
+			})
+		})
+	})
 }
 
 func prepareDB() (string, *storm.DB) {
-	dir, _ := ioutil.TempDir(os.TempDir(), "storm")
+	dir := filepath.Join(os.TempDir(), "feconnector-test")
+	os.MkdirAll(dir, os.ModePerm)
 	db, _ := storm.Open(filepath.Join(dir, "storm.db"))
+	os.Mkdir(filepath.Join(dir, "logs"), os.ModePerm)
+	os.Mkdir(filepath.Join(dir, "notes"), os.ModePerm)
 	for _, model := range logFiles {
 		db.Save(&model)
+	}
+	for _, model := range logFiles {
+		logName := model.Name + "-" + time.Unix(model.Timestamp, 0).Format("2006-01-02-15-04-05") + ".txt"
+		f, _ := os.Create(filepath.Join(dir, "logs", logName))
+		f.Close()
+		if model.HasNote {
+			ioutil.WriteFile(filepath.Join(dir, "notes", logName), []byte("test"), os.ModePerm)
+		}
 	}
 	return dir, db
 }
