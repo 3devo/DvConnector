fc5763dcde4fbc1a41ee30b121d3c4ac0d37e5c5
Bob Thomas <bobthomas295@gmail.com>
2018-07-19 11:52:26 +0200

    MODEL: Add better validation to user model

--- models/user.go
+++ models/user.go
@@ -2,7 +2,7 @@ package models
 
 type User struct {
 	UUID            string `storm:"id" json:"uuid" validate:"uuid"`
-	Username        string `json:"username" storm:"unique" validate:"email"`
-	Password        string `json:"password" validate:"min=8"`
+	Username        string `json:"username" storm:"unique" validate:"required,email"`
+	Password        string `json:"password" validate:"required,min=8"`
 	TrackingAllowed bool   `json:"trackingAllowed"`
 }
