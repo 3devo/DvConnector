66d9b223a189d20437ff76ecf121fe674bf5eece
Bob Thomas <bobthomas295@gmail.com>
2019-01-16 15:36:14 +0100

    RESOURCE: Add configuration resource with routing

---
+++ models/config.go
@@ -0,0 +1,8 @@
+package models
+
+// Config model that keep tracks of application configuration
+// swagger:model ConfigResponse
+type Config struct {
+	ID          int  `storm:"id,increment" json:"id"`
+	OpenNetwork bool `json:"openNetwork"`
+}
